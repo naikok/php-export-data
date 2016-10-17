cb8eac84b5faa37e6e9c761555883d557cbb666c
Sergio <sergiogayarre@gmail.com>
2016-10-17 15:37:50 +0200

    Update ExportDataInterface.php

--- ManagerTypes/ManagerInterface/ExportDataInterface.php
+++ ManagerTypes/ManagerInterface/ExportDataInterface.php
@@ -1,23 +1,9 @@
 <?php
 
-interface ExportDataInterface {
-
-   // public function initialize();
-
-    public function generateFooter();
-
-    public function generateHeader();
-
-    public function generateRow($row);
-
-    //public function sendHttpHeaders();
-
-    //public function write($data);
-
-   // public function addRow($row);
-
-    public function getType();
-
-   // public function finalize();
-
+interface ExportDataInterface 
+{
+   public function generateFooter();
+   public function generateHeader();
+   public function generateRow($row);
+   public function getType();
 }
